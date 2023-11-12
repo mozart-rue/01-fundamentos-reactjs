fdef85dcca1b51ebadc56198a7ae9734b8be4105
Mozart Rueckert <mozartrue@outlook.com>
2023-11-12 20:22:10 -0300

    initial commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,49 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
+
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/mozart-rue.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Mozart Rueckert</strong>
+              <time title="11 de maio as 8:15h" dateTime="2022-05-11 08:15:32">
+                Cerca de 1h há atras
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentario">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
